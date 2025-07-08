72742a6f8bc18a3351dd2c4a15f2d0b5276b2cb6
Joo <joo@joostory.net>
2025-07-08 22:38:46 +0900

    본문 크기가 줄어들지 않도록 수정

--- src/components/layout/Content.tsx
+++ src/components/layout/Content.tsx
@@ -6,15 +6,10 @@ type ContentProps = {
 
 export default function Content({ children }: ContentProps) {
   return (
-    <div
-      className={
-        `absolute inset-0 overflow-y-auto font-serif
-        md:left-60
-        lg:max-w-2xl lg:rounded-xl lg:shadow-lg lg:top-5 lg:bottom-5 lg:left-1/2 lg:-translate-x-1/2 lg:ml-30
-        lg:bg-base-300 lg:p-5`
-      }
-    >
-      {children}
+    <div className="absolute inset-0 font-serif md:left-60 flex justify-center lg:py-5 lg:dark:bg-base-200">
+      <div className="overflow-y-auto lg:w-2xl lg:rounded-xl lg:shadow-lg bg-base-100 lg:p-5">
+        {children}
+      </div>
     </div>
   )
 }
