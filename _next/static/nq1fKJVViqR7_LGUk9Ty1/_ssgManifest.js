bdb05bac6fb9a03495807cf1440ed5f457b99546
joostory <joo@joostory.net>
2025-07-27 02:49:19 +0000

    Deploying to gh-pages from @ joostory/holybible@880c2ca4a67967f6269d86ab44c3e64d4e26ada3 🚀

---
+++ _next/static/nq1fKJVViqR7_LGUk9Ty1/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002F[version]","\u002F[version]\u002F[bible]","\u002F[version]\u002F[bible]\u002F[chapter]","\u002Fpage\u002Fov"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
